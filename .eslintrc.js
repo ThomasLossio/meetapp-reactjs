99ea7c7b38af6f81f3ce9eb6d8caa3a1f0223e98
thomasneo <thomaslossio@hotmail.com>
2019-09-15 16:40:47 -0300

    Redux and Redux Saga Set Up. Auth and User stores set up. It is already authenticating and storing auth and user's info

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
   },
   settings: {
     'import/resolver': {
